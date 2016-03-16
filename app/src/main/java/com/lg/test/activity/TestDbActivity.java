7bd39ef1be270f9ec01ed03b89048a20bbb4fa63
liguo <717693247@qq.com>
2016-03-16 17:26:55 +0800

    --

--- app/src/main/java/com/lg/test/activity/TestDbActivity.java
+++ app/src/main/java/com/lg/test/activity/TestDbActivity.java
@@ -3,35 +3,29 @@ package com.lg.test.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.lg.base.core.ActionBarMenu;
-import com.lg.base.core.InjectView;
 import com.lg.base.utils.StringUtil;
 import com.lg.test.R;
 import com.lg.test.core.SuperActivity;
 import com.lg.test.task.NoteAddTask;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * Created by liguo on 2015/10/14.
  */
-public class TestDbActivity extends SuperActivity{
+public class TestDbActivity extends SuperActivity {
 
-    @InjectView(R.id.act_note_add_text)
-    EditText etNoteText;
+    @Bind(R.id.act_note_add_text)
+    EditText actNoteAddText;
 
-    @InjectView(value = R.id.act_note_add_btn_ok,click = "onClick")
-    Button btnAdd;
-
-    public static Intent createIntent(Context ctx){
-        Intent it = new Intent(ctx,TestDbActivity.class);
+    public static Intent createIntent(Context ctx) {
+        Intent it = new Intent(ctx, TestDbActivity.class);
         return it;
-    }
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -44,13 +38,19 @@ public class TestDbActivity extends SuperActivity{
         return R.layout.activity_note_add;
     }
 
-    public void onClick(View v) {
-        if(v == btnAdd){
-            String text = etNoteText.getText().toString().trim();
-            if(StringUtil.isEmpty(text)){
-                return;
-            }
-            new NoteAddTask(this,text){}.execute();
+    @OnClick(R.id.act_note_add_btn_ok)
+    public void onClick() {
+        String text = actNoteAddText.getText().toString().trim();
+        if (StringUtil.isEmpty(text)) {
+            return;
         }
+        new NoteAddTask(this, text) {
+        }.execute();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
     }
 }
